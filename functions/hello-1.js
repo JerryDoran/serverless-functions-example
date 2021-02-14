69331de1aa2a5f9d034ad3d2a84ebe6d967395f0
Jerome Doran <disneyfan9184@gmail.com>
2021-02-14 09:57:47 -0500

    feat: added headers to serverless functions for outside data access

--- functions/hello-1.js
+++ functions/hello-1.js
@@ -1,6 +1,9 @@
 exports.handler = async (event, context) => {
   console.log(event);
   return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     statusCode: 200,
     body: 'My first Netlify function!',
   };
